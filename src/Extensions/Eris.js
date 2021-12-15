e658b169eaf12bf999ae66aa57be7e0134547f42
ThatBlueDevil <thatbluedevil3@gmail.com>
2021-12-15 18:06:35 -0500

    Whoops

--- src/Extensions/Eris.js
+++ src/Extensions/Eris.js
@@ -16,20 +16,15 @@ module.exports = class Eris {
     }
 
     async getGuild(guildId) {
-        if (!guildId) return;
         return this.client.guilds.get(guildId) ?? this.client.guilds.add(await this.client.getRESTGuild(guildId));
     }
 
     async getUser(userId) {
-        if (!userId) return;
         return this.client.users.get(userId) ?? this.client.users.add(await this.client.getRESTUser(userId));
     }
 
     async getMember(guildId, memberId) {
-        if (!guildId || !memberId) return;
         const guild = await this.getGuild(guildId);
-        if (!guild) return;
-
         return guild.members.get(memberId) ?? guild.members.add(await this.client.getRESTGuildMember(guildId, memberId));
     }
 }
