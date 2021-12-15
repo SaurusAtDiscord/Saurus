1ea35923f502aba543a4b21174858852a6922792
ThatBlueDevil <thatbluedevil3@gmail.com>
2021-12-15 10:08:01 -0500

    Support other interactions

--- src/Events/interactionCreate.js
+++ src/Events/interactionCreate.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 'use strict';
 
 const Event = require('@core/Event');
@@ -5,21 +6,26 @@ const Embed = require('@units/Embed');
 
 module.exports = class interactionCreate extends Event {
     async execute(interaction) {
-        if (interaction.constructor.name !== 'CommandInteraction') return;
+        switch(interaction.constructor.name) {
+            case 'CommandInteraction':
+                const command = this.client.commands.find(cmd => cmd.name === interaction.data.name.toLowerCase());
+                if (!command) return;
         
-        const command = this.client.commands.find(cmd => cmd.name === interaction.data.name.toLowerCase());
-        if (!command) return;
-
-        await interaction.defer(interaction.data.name === 'help' ? 64 : undefined);
+                await interaction.defer(interaction.data.name === 'help' ? 64 : undefined);
+                
+                if (command.userPermissions) {
+                    const perms = interaction.channel.permissionsOf(this.client.user.id);
+                    const missingUserPerms = command.userPermissions.filter(perm => !perms.has(perm));
+                    return interaction.createFollowup(new Embed({ description: missingUserPerms }));
+                }
         
-        if (command.userPermissions) {
-            const perms = interaction.channel.permissionsOf(this.client.user.id);
-            const missingUserPerms = command.userPermissions.filter(perm => !perms.has(perm));
-            return interaction.createFollowup(new Embed({ description: missingUserPerms }));
+                const args = {};
+                interaction.data.options?.forEach(input => args[input.name] = input.value);
+                command.execute(interaction, args);
+                break;
+            case 'ComponentInteraction':
+                //console.log(interaction);
+                break;
         }
-
-        const args = {};
-        interaction.data.options?.forEach(input => args[input.name] = input.value);
-        command.execute(interaction, args);
     }
 }
