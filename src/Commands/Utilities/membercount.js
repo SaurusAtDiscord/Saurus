fe8ced41f75775e7d3b0ba9cecbcc0399deb5714
ThatBlueDevil <thatbluedevil3@gmail.com>
2022-01-22 05:22:33 -0500

    Changed Values

--- src/Commands/Utilities/membercount.js
+++ src/Commands/Utilities/membercount.js
@@ -13,9 +13,11 @@ module.exports = class Membercount extends Command {
     /* Calling the method "execute" on Command class. */
     async execute(interaction) {
         const guild = await this.client.utils.getGuild(interaction.guildID);
+        const members = await guild.fetchMembers();
+
         return interaction.createFollowup({ embed: { fields: [
-            { name: 'Total Members', value: await guild.fetchAllMembers(), inline: true },
-            { name: 'Bots', value: guild.members.filter(m => m.bot).length, inline: true }
+            { name: 'Members', value: members.filter(m => !m.bot).length, inline: true },
+            { name: 'Bots', value: members.filter(m => m.bot).length, inline: true }
         ]}});
     }
 }
