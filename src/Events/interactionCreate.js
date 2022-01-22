87f00e997e7230e31c6131f7eb0406356027cc1a
ThatBlueDevil <thatbluedevil3@gmail.com>
2022-01-22 05:32:38 -0500

    not much of a change

--- src/Events/interactionCreate.js
+++ src/Events/interactionCreate.js
@@ -7,19 +7,22 @@ module.exports = class interactionCreate extends Event {
 
     /* Calling the method "execute" on Event class. */
     async execute(interaction) {
-        if (interaction.constructor.name !== 'CommandInteraction') return;
-        const command = this.client.commands.find(cmd => cmd.name === interaction.data.name?.toLowerCase());
-        if (!command) return;
+        if (interaction.constructor.name === 'CommandInteraction') {
+            const command = this.client.commands.find(cmd => cmd.name === interaction.data.name?.toLowerCase());
+            if (!command) return;
 
-        await interaction.acknowledge(command.name === 'help' && Constants.MessageFlags.EPHEMERAL);
-        // if (command.userPermissions) {
-        //     const perms = interaction.channel.permissionsOf(this.client.user.id);
-        //     const missingUserPerms = command.userPermissions.filter(perm => !perms.has(perm));
-        //     return interaction.createFollowup({ embed: { description: missingUserPerms }});
-        // }
+            await interaction.acknowledge(command.name === 'help' && Constants.MessageFlags.EPHEMERAL);
+            // if (comma) {
+            //     const perms = interaction.channel.permissionsOf(this.client.user.id);
+            //     const missingPerms = command.permissions.filter(perm => !perms.has(perm));
+            //     const missingUserPerms = command.userPermissions.filter(perm => !perms.has(perm));
 
-        const args = {};
-        interaction.data.options?.forEach(input => args[input.name] = input.value);
-        command.execute(interaction, args);
+            //     if (missingUserPerms.length) return interaction.createFollowup({ embed: { description: missingUserPerms }});
+            // }
+
+            const args = {};
+            interaction.data.options?.forEach(input => args[input.name] = input.value);
+            command.execute(interaction, args);
+        }
     }
 }
