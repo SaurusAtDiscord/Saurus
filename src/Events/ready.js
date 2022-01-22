3e157485e61ef148d630b1d66299bc833a1c368f
ThatBlueDevil <thatbluedevil3@gmail.com>
2022-01-22 05:32:25 -0500

    Add bot status and get rid of useless line

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Event = require('@core/Event');
-const { isEqual } = require('lodash');
 
 module.exports = class ready extends Event {
 	
@@ -11,10 +10,17 @@ module.exports = class ready extends Event {
 
 		const newCommands = this.client.commands.filter(command => !clientCommands.some(cmd => cmd.name === command.name));
 		const deletedCommands = clientCommands.filter(command => !this.client.commands.find(cmd => cmd.name === command.name));
-		const changedCommands = clientCommands.filter(command => !this.client.commands.some(cmd => isEqual(command.options, cmd.options)));
-
+		const changedCommands = clientCommands.filter(command => !this.client.commands.some(cmd => (command.description === cmd.description) || (JSON.stringify(command.options) === JSON.stringify(cmd.options))));
+		
+		if (deletedCommands.length || newCommands.length) this.client.utils.createCommand(this.client.commands);
 		if (changedCommands.length) changedCommands.forEach(command => this.client.utils.editCommand(this.client.commands.find(cmd => cmd.name === command.name), command.id));
-		if (deletedCommands.length) deletedCommands.forEach(cmd => this.client.utils.deleteCommand(cmd.id));
-		if (newCommands.length) this.client.utils.createCommand(this.client.commands);
+		
+		setInterval(() => {
+            const statuses = [ `over ${this.client.guilds.size ?? 'NaN'} guilds`, 'zzz' ]
+            this.client.editStatus('idle', {
+                name: `${statuses[Math.floor(Math.random() * statuses.length)]}`,
+                type: 3
+            });
+        }, 30000);
 	}
 }
