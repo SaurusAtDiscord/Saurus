2dc993e815cb12d3e2099df8432ce526e80946b8
ThatBlueDevil <thatbluedevil3@gmail.com>
2022-01-26 12:26:35 -0500

    Use Template literals.

--- src/Commands/General/whois.js
+++ src/Commands/General/whois.js
@@ -22,13 +22,14 @@ module.exports = class Whois extends Command {
     async execute(interaction, args) {
         const user = (args.user && await this.client.utils.getMember(interaction.guildID, args.user)) ?? interaction.member;
         const roles = user.roles && user.roles.map(role => user.guild.roles.get(role)).sort((a, b) => b.position - a.position).map(role => `<@&${role.id}>`);
-
+        const stringedRole = roles.length ? `\n• Roles: ${roles.join(' ')}` : '';
+        
         return interaction.createFollowup({ embed: {
             author: { name: `${user.username}#${user.discriminator}`, icon_url: user.avatarURL },
             fields: [
                 {
                     name: 'Guild Details',
-                    value: `• Nickname: ${user.nick ?? 'No Nickname'}\n• Joined at: ${moment(new Date(user.joinedAt)).format('LL')} (\`${moment(user.joinedAt).fromNow()}\`)${roles.length ? '\n• Roles: ' + roles.join(' ') : ''}`
+                    value: `• Nickname: ${user.nick ?? 'No Nickname'}\n• Joined at: ${moment(new Date(user.joinedAt)).format('LL')} (\`${moment(user.joinedAt).fromNow()}\`)${stringedRole}`
                 },
                 {
                     name: 'User Details',
