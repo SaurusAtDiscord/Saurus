5fc3b81aecd38c307aa677d741785333674c78f0
ThatBlueDevil <70230904+ThatBlueDevil@users.noreply.github.com>
2022-02-14 23:57:51 -0500

    Delete src directory

--- src/init.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('module-alias/register');
-require('dotenv').config();
-
-/* The code below is creating a new Saurus client and then initiating it. */
-const Saurus = require('@core/Client');
-new Saurus(process.env.KEY).initiate();
