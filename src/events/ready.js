c54e33b291fba55b3d867a36baa48a0a803d2466
ThatBlueDevil <thatbluedevil3@gmail.com>
2022-03-03 09:08:41 -0800

    Check for description changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -12,12 +12,12 @@ module.exports = class ready extends Event {
 
 		const newCommands = this.client.commands.filter(command => !clientCommands.some(cmd => cmd.name === command.name));
 		const deletedCommands = clientCommands.filter(command => !this.client.commands.find(cmd => cmd.name === command.name));
-		const changedCommands = clientCommands.filter(command => !this.client.commands.some(cmd => isEqual(command.options, cmd.options)));
+		const changedCommands = clientCommands.filter(command => !this.client.commands.some(cmd => isEqual({ description: command.description, options: command.options }, { description: cmd.description, options: cmd.options })));
 
 		if (clientCommands.length === 0) this.client.utils.createCommand(this.client.commands);
 		if (deletedCommands.length || newCommands.length) this.client.utils.createCommand(this.client.commands);
 		if (changedCommands.length) changedCommands.forEach(command => this.client.utils.editCommand(this.client.commands.find(cmd => cmd.name === command.name), command.id));
-		
+
 		console.log(`[${this.client.user.username}] Online~!`);
 		setInterval(() => {
             const statuses = [ `over ${this.client.guilds.size ?? 'NaN'} guilds`, 'zzz' ]
